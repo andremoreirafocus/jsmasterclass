b80c1d9d7ff8b162ea10e128b60c9f38260812ad
Andre Moreira <andre.moreira@focus-sys.com.br>
2022-07-01 15:02:41 -0300

    add: json methods

---
+++ json.js
@@ -0,0 +1,8 @@
+console.log(JSON.stringify(10));
+console.log(JSON.stringify("java"));
+console.log(JSON.stringify({ name: "Javascript", paradigm: "OO" }));
+console.log(typeof JSON.parse("10"));
+console.log(typeof JSON.parse('"Javascript"'));
+console.log(JSON.parse('{"name": "Javascript", "paradigm": "OO"}'));
+console.log(JSON.parse("[1,2,3,4,5]"));
+console.log(JSON.parse("null"));
