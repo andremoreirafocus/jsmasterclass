5beb0d1e6be276accdd5dca7d39ba1389427134a
Andre Moreira <andre.moreira@focus-sys.com.br>
2022-07-01 15:55:30 -0300

    Add: string API

---
+++ strings.js
@@ -0,0 +1,8 @@
+// let b = new Number(10);
+let b = 10;
+console.log(b);
+console.log(typeof b);
+// let str = new String("sadfsd");
+let str = "sadfsd";
+console.log(str);
+console.log(typeof str);
